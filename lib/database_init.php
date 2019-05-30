42dbf7388df5c72f2840a873ad5953c7180c6f7a
semseysandor <51154903+semseysandor@users.noreply.github.com>
2019-05-30 10:08:14 +0200

    Add files via upload

---
+++ lib/database_init.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * DataBase init
+ *********************************************************/
+
+$db_host =		 'localhost';
+$db_username = 'leltar_USER';
+$db_pass =		 'leltar';
+$db_name =		 'inventory';
+
+// Connect to SQL server
+try {
+
+	// Open a link to the database
+	$link = new mysqli($db_host, $db_username, $db_pass, $db_name);
+
+	// If error in connection
+	if ($link->connect_error) {
+		throw new leltar_exception('no_conn_db',1);
+	}
+
+} catch (leltar_exception $e) {$e->error_handling();}
+
+// Show all errors: DEVELOPMENT ONLY
+//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// Don't show errors: PRODUCTION ONLY
+mysqli_report(MYSQLI_REPORT_OFF);
+
+// Set character set
+$link->set_charset('utf8');?>
---
+++ lib/event_handlers.php
@@ -0,0 +1,166 @@
+<?php
+/**
+ * JavaScript event handlers
+ *********************************************************/
+
+/**
+ * Returns JavaScript code
+ *
+ * @param		string		$select
+ * @param		array			$p			optional parameter list
+ *
+ * @return	string		JS code
+ */
+function js($select, array $p = NULL) {
+
+	$retr = 'onclick="retrieveData(\'exec/retrieve.php?q=';
+	$prevent = 'onsubmit="event.preventDefault();';
+
+	switch ($select) {
+
+		case 'get':
+			$js = $retr;
+			break;
+
+		case 'index_drop':
+			$js = $retr;
+			break;
+
+		case 'submit':
+			$js = $prevent.'submitForm(this, \'msg_center\', retrieveData, \'exec/retrieve.php?q=';
+			break;
+
+		case 'upload':
+			$js = $prevent.'submitFormUpload(this, \'msg_center\', retrieveData, \'exec/retrieve.php?q=';
+			break;
+
+		case 'menu':
+			$js = $retr;
+			break;
+
+		default:
+			return FALSE;
+	}
+
+	if ($select == 'index_drop') {
+		$js .= $p[0].'\', \''.$p[1].'\');';
+		$js .= 'dropDown(\''.$p[1].'\')"';
+	} elseif ($select == 'menu') {
+		$js .= $p[0].'\', \'index\');';
+		$js .= 'closeAllOpened()"';
+	} else {
+		$js .= $p[0].'\', \''.$p[1].'\')"';
+	}
+
+	return $js;
+}
+
+/**
+ * Returns JavaScript code
+ *
+ * @param		string		$select
+ * @param		array			$parameters	optional parameter list
+ *
+ * @return	string		JS code
+ */
+function js_get_form($select, array $parameters = NULL) {
+
+	$js = 'onclick="retrieveData(\'exec/retrieve_form.php?q=';
+	$js .= $select;
+
+	if (is_array($parameters)) {
+		foreach ($parameters as $p) {
+			$js .= '&'.$p;
+		}
+	}
+
+	$js .= '\', \'popup\')"';
+
+	return $js;
+}
+
+/**
+ * Returns JavaScript code
+ *
+ * @param		string		$select
+ * @param		array			$p	optional parameter list
+ *
+ * @return	string		JS code
+ */
+function js_spec($select, array $p = NULL) {
+
+	$retr = 'onclick="retrieveData(\'exec/';
+	$exec = 'onclick="executeServer(\'exec/';
+	$prevent = 'onsubmit="event.preventDefault();';
+	$submit = 'submitForm(this, \'msg_center\',';
+	$close = 'closeAllOpened();';
+
+	switch ($select) {
+
+	// Dropdown
+	case 'drop':
+		$js = 'onclick="dropDown(\''.$p[0].'\')"';
+		break;
+
+	// Retrieve data
+	case 'compound':
+		$js = $retr.'retrieve.php?q=compound_det&cid='.$p[0].'\', \'popup\', getSmiles, \'compID\', '.$p[0].')"';
+		break;
+
+	case 'barcode':
+		$js = $retr.'search.php?q='.$p[0].'\', \'index\', getSmiles, \'barcode\', \''.$p[0].'\');';
+		$js .= $close.'"';
+		break;
+
+	// Get compound info from CAS
+	case 'calc_cas':
+		$js = 'onclick="event.preventDefault();getCompoundInfo()"';
+		break;
+
+	// Execute
+	case 'login':
+		$js = $prevent.$submit.'redirect, \'index.php\')"';
+		break;
+
+	case 'logout':
+		$js = $exec.'logout.php\', \'msg_center\', redirect, \'index.php\')"';
+		break;
+
+	case 'inactivate':
+		$js = 'onclick="alert(\'really\')"';
+		//$js = $exec.'inactivate.php?bid='.$p[0].'&pid='.$p[1].'\', \'msg_center\',';
+		//$js .= 'retrieveData, \'exec/search.php?q='.$p[2].'\', \'index\')"';
+		break;
+
+	// Forms
+	case 'search_form':
+		$js = $prevent.$submit.'retrieveData, \'exec/search.php?q='.$p[0].'\', \'index\', getSmiles, \'barcode\', \''.$p[0].'\')"';
+		break;
+
+	case 'search':
+		$js = $prevent.'retrieveData(\'exec/search.php?q=\' + this.q.value, \'index\', getSmiles, \'barcode\', this.q.value);';
+		$js .= $close.'"';
+		break;
+
+	// Random
+	case 'erase_popup':
+		$js = 'onclick="erasePopup()"';
+		break;
+
+	// Change history
+	case 'chng_hist':
+		$js = 'onclick="getChangeHistorySummary('.$p[0].', \''.$p[1].'\')"';
+		break;
+
+	case 'chng_hist_det':
+		$js = 'onclick="getChangeHistoryDetails('.$p[0].',\''.$p[1].'\','.$p[2].')"';
+		break;
+
+	// Default
+	default:
+		return FALSE;
+	}
+
+	return $js;
+}
+?>
