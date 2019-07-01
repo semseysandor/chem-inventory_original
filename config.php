1dff23e5fd26027533e65dc47951a0564db04a49
semseysandor <semseysandor@gmail.com>
2019-07-01 15:17:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,13 +5,13 @@
 
 // DB login info
 
-define('DB_HOST', 'localhost');		# SQL server host name
-define('DB_USER', 'leltar_USER');	# SQL user name
-define('DB_PASS', 'leltar');			# password
+define('DB_HOST', 'localhost');		# SQL user name
+define('DB_USER', 'DBuser');			# SQL server host name
+define('DB_PASS', 'password');		# password
 define('DB_NAME', 'inventory');		# database name
 
 // LDAP server
-define('LDAP_HOST', '192.168.10.1');								# LDAP host
-define('LDAP_DN', 'CN=Users,DC=nangenex,DC=local');	# LDAP dn
-define('LDAP_USR_DOM', '@nangenex.local');					# LDAP user domain
+define('LDAP_HOST', 'your_host');						# LDAP host
+define('LDAP_DN', 'your_dn');								# LDAP dn
+define('LDAP_USR_DOM', 'your_user_domain');	# LDAP user domain
 ?>
