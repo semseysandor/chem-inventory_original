671f1fb619f49f06e19a0d2c352ffbef22122397
semseysandor <semseysandor@gmail.com>
2019-09-03 11:01:30 +0200

    Update footer.php

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-	<div><strong>LelTár</strong> v2.0</div>
+	<div><strong>LelTár</strong> v2.1</div>
 	<div>
 		<div class="rwd">Fejlesztette </div>
 		<div class="rwd"><em>Semsey Sándor</em></div>
